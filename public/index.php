f18fec591ad13846a5b943544a4bac7c3d327b1e
unknown <zahiri1979@gmail.com>
2022-05-31 10:13:16 +0200

    premier commit

---
+++ public/index.php
@@ -0,0 +1,97 @@
+<!-- <?php
+
+        include('../models/function.php');
+        ?> -->
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/274ac46116.js" crossorigin="anonymous"></script>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="../models/style.css">
+    <link rel="shortcut icon" href="https://firebasestorage.googleapis.com/v0/b/lys1-e9cd8.appspot.com/o/logos%2Ffoodexpress.png?alt=media&token=f193dfc3-cc60-4f9f-b180-eb623850e74f" type="image/x-icon">
+    <title>Burger Company</title>
+</head>
+
+<body>
+    <header>
+        <!-- NAVBAR-->
+        <nav class="navbar navbar-expand-lg py-3 navbar-dark bg-dark shadow-sm">
+            <div class="container">
+                <a href="#" class="navbar-brand">
+                    <!-- Logo Image -->
+                    <img src="https://firebasestorage.googleapis.com/v0/b/lys1-e9cd8.appspot.com/o/logos%2Ffoodexpress.png?alt=media&token=f193dfc3-cc60-4f9f-b180-eb623850e74f" width="40" alt="" class="d-inline-block align-middle mr-2">
+                    <!-- Logo Text -->
+                    <span class="text-uppercase font-weight-bold">Burger Company</span>
+                </a>
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarToggleExternalContent5" aria-controls="navbarToggleExternalContent5" aria-expanded="false" aria-label="Toggle navigation">
+                    </button>
+                </div>
+
+                <!-- <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button> -->
+
+                <div id="navbarSupportedContent" class="collapse navbar-collapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active"><a href="#" class="nav-link">Accueil <span class="sr-only"></span></a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Panier</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Commande</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div class="text-white cover" id="cover">
+        <div class="px-sm-5 py-sm-4 text-white">
+            <div class="row h-100 mt-sm-0 justify-content-center">
+                <div class="row text-center mt-sm-0 ">
+                    <div class="col my-auto px-3 px-sm-5">
+                        <p class="text-white text-title cover-title mb-0 false">Burger Company</p>
+                        <div class="px-lg-5 px-0">
+                            <p class="line-break cover-description mb-3 px-lg-5 px-0"> Commande vos plats en quelques clics !</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <footer>
+        <!-- NAVBAR-->
+        <nav class="navbar navbar-expand-lg py-3 navbar-dark bg-dark shadow-sm">
+            <div class="container">
+
+                <a href="#" class="navbar-brand">
+                    <!-- Logo Image -->
+                    &copy; Loic & Mustapha
+                    </i>
+                </a>
+                <a href="#" class="navbar-brand">
+                    <!-- Logo Image -->
+                    <i class='fab fa-facebook fa-2x'></i><br />
+
+                </a>
+
+                <a href="#" class="navbar-brand">
+                    <!-- Logo Image -->
+                    <i class='fab fa-instagram fa-2x'></i>
+                    </i>
+                </a>
+
+
+            </div>
+        </nav>
+
+    </footer>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
+</html>
